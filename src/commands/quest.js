18d094ba13476b43a95e0718fb5132de559c592e
boydbloemsma <boydbloemsma@gmail.com>
2020-03-02 15:43:00 +0100

    :neckbeard: Starting laying the groundworks for webscraping

--- src/commands/quest.js
+++ src/commands/quest.js
@@ -1,4 +1,23 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
 module.exports = async (msg, args) => {
   const addOn = args.join('+');
-  await msg.channel.send(`https://divinityoriginalsin2.wiki.fextralife.com/${addOn}`);
+  const url = `https://divinityoriginalsin2.wiki.fextralife.com/${addOn}`;
+
+  rp(url).then((html) => {
+    const questHeaders = [];
+    const objectives = [];
+    const textFields = [];
+    $('div#wiki-content-block > h3', html).map((i, elm) => questHeaders.push($(elm).text()));
+    $('div#wiki-content-block > ol > li', html).map((i, elm) => objectives.push($(elm).text()));
+    $('div#wiki-content-block > p', html).map((i, elm) => textFields.push($(elm).text()));
+
+    for (let i = 8; i < (textFields.length - 2); i++) {
+      console.log(textFields[i]);
+    }
+    // console.log(objectives);
+  });
+
+  // await msg.channel.send(`https://divinityoriginalsin2.wiki.fextralife.com/${addOn}`);
 };
