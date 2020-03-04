b29de77a2a9cff6eb3136aec35b476a4528a57ac
boydbloemsma <boydbloemsma@gmail.com>
2020-03-04 11:39:25 +0100

    :construction: Added the important NPCs field

--- src/commands/quest.js
+++ src/commands/quest.js
@@ -7,13 +7,16 @@ module.exports = (msg, args) => {
 
   rp(url).then((html) => {
     const questHeaders = [];
+    const importantNPCs = [];
     const objectives = [];
     let textFields = [];
     $('div#wiki-content-block > h3', html).map((i, elm) => questHeaders.push($(elm).text()));
+    $('div#wiki-content-block > ul > li > a', html).map((i, elm) => importantNPCs.push($(elm).text()));
     $('div#wiki-content-block > ol > li', html).map((i, elm) => objectives.push($(elm).text()));
     $('div#wiki-content-block > p', html).map((i, elm) => textFields.push($(elm).text()));
     textFields = textFields.filter((el) => /\S/.test(el)).slice(1).join('\n\n');
 
+    console.log(importantNPCs);
     const embed = {
       color: 0xA63D40,
       title: 'Quest helper!',
@@ -21,7 +24,7 @@ module.exports = (msg, args) => {
       fields: [
         {
           name: `${questHeaders[0]}`,
-          value: 'testing',
+          value: `${importantNPCs.join('\n')}`,
           inline: true,
         },
         {
